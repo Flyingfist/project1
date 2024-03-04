b12145a23f386e39ac4159aa6d036f07292d79c8
Juri <juri.aristov@gmail.com>
2024-03-05 00:19:44 +0300

    Vikulka's tasks

--- js/script.js
+++ js/script.js
@@ -10,13 +10,61 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?",""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?","");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?","");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log("Done@");
+    } else {
+        console.log("Err0r");
+        i--;
+    }    
+}
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Ошибочка");
+}
 
 console.log(personalMovieDB);
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 20;
+    return num;
+}
+
+// showFirstMessage('приветики');
+console.log(showFirstMessage('приветики'));
+
+let asd = 4;
+
+console.log(--asd);
+console.log(++asd);
+console.log(asd--);
+console.log(asd++);
+
+
+function returnNeighboringNumbers(number) {
+    let arr = [--number, number, ++number];    
+       return arr;
+    }
+console.log(returnNeighboringNumbers(4));
+
+function getMathResult(a, b) {
+    let result  = '';
+    for  ( let i = 0; i < b; i++) {
+        result = result + (a * (i + 1));
+        if (i != (b - 1)) {
+            result += '---';
+        }
+    }
+    return result;
+}
+console.log(getMathResult(5, 7));
